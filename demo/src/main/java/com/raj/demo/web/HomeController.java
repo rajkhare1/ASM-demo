b32a365ac88399efb861349a7a56fc14e9712843
RK00333040 <RK00333040@TMIC-LPT013815.TMHaier.ad>
2019-12-05 12:36:42 +0530

    update for thymeleaf example

---
+++ demo/src/main/java/com/raj/demo/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.raj.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.raj.demo.domain.User;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("formData",new User());
+		return "index";
+	}
+	
+	@RequestMapping(value="/create",method=RequestMethod.POST)
+	public String processFormData(User user) {
+		return "result";
+	}
+}
