1bc57ffb2fef9cfe16ccd67fcd87277114de7a73
RK00333040 <RK00333040@TMIC-LPT013815.TMHaier.ad>
2019-12-05 17:02:13 +0530

    Update for RedirectAttributes

--- demo/src/main/java/com/raj/demo/web/HomeController.java
+++ demo/src/main/java/com/raj/demo/web/HomeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.raj.demo.domain.User;
 
@@ -17,7 +18,15 @@ public class HomeController {
 	}
 	
 	@RequestMapping(value="/create",method=RequestMethod.POST)
-	public String processFormData(User user) {
+	public String processFormData(User user, RedirectAttributes attr) {
+		//Insert data submitted in the form to database
+		
+		attr.addFlashAttribute("user",user);
+		return "redirect:/display";
+	}
+	
+	@RequestMapping(value="/display", method=RequestMethod.GET)
+	public String displayFormData(User user) {
 		return "result";
 	}
 }
